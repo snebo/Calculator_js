52a313ff9294f4c645e6e441a9e75474dd378aa0
Stefano <snebo54@hotmail.com>
2023-11-10 20:02:29 +0100

    Added button press now reflects on screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,41 @@
 //imports
-
+const numberBtns = document.querySelectorAll(".numBtn")
+const inputBox = document.querySelector(".func")
+const operatorBtns = document.querySelectorAll(".oprBtn")
 
 //functions
 function add(a,b){return a+b}
 function substract(a,b){return a-b}
 function multiply(a,b){return a*b}
 function divide(a,b){return a/b}
-
-function setOperator(symbol){
-    if(symbol = '*'){return multiply(firstNumber, lastNumber)}
-    else if (symbol = '/'){return divide(firstNumber, lastNumber)}
-    else if (symbol = '+'){return add(firstNumber, lastNumber)}
-    else if (symbol = '-'){return substract(firstNumber, lastNumber)}
+function setNumber(number){
+    inputBox.textContent+= number
+}
+function setOperator(sym){
+    if(sym == "+"){
+        inputBox.textContent+= `${sym}`}
+    else if(sym == "-"){
+        inputBox.textContent+= `${sym}`}
+    else if(sym == "x"){
+        inputBox.textContent+= `${sym}`}
+    else if(sym == "÷"){
+        inputBox.textContent+= `${sym}`}
 }
 
+// function setOperator(symbol){
+//     if(symbol = '*'){return multiply(firstNumber, lastNumber)}
+//     else if (symbol = '/'){return divide(firstNumber, lastNumber)}
+//     else if (symbol = '+'){return add(firstNumber, lastNumber)}
+//     else if (symbol = '-'){return substract(firstNumber, lastNumber)}
+// }
+
 let firstNumber, lastNumber, operator;
+
+numberBtns.forEach(numberBtn => numberBtn.addEventListener('click', ()=>{
+    setNumber(Number(numberBtn.textContent))
+}))
+operatorBtns.forEach(operatorbtn => operatorbtn.addEventListener('click',()=>{
+    setOperator(operatorbtn.textContent)
+}))
+
+
